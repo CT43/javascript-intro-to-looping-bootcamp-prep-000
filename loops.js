110c322e556f31121dccbb99a53642858981cc1a
christian.thomas.2894@gmail.com <christian.thomas.2894@gmail.com>
2018-04-25 18:54:55 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,10 +2,10 @@
 function forLoop(array) {
  for (let i = 0; i < 25; i++) {
   if(i===1) {
-   "I am 1 strange loop"
+   array.push("I am 1 strange loop")
   }
   else {
-   "I am" + array +"strange loops"
+   array.push("I am" + array +"strange loops")
   } 
   return array
 }
